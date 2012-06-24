7702cd13d8e3b023bbe72b189eb4af088c4846fe
borislav <b.manolov@gmail.com>
2012-06-24 13:15:46 +0200

    use PDO instead of the phpbb sql routines by the static log generation for the chat

--- chat/maintenance/generate_static_logs.php
+++ chat/maintenance/generate_static_logs.php
@@ -1,7 +1,11 @@
 <?php
+// Show all errors:
+error_reporting(E_ALL);
+
 require_once dirname(__FILE__) . '/generate_static_logs.inc';
 
-#$endDate = isset($argv[1]) ? $argv[1] : date('Y-m-d');
-#generateFilesForPeriod(START_CHAT_DATE, $endDate);
+$generator = new StaticLogGenerator(getDb());
+$generator->generateNewLogs();
 
-generateNewLogs();
+#$endDate = isset($argv[1]) ? $argv[1] : date('Y-m-d');
+#$generator->generateFilesForPeriod(StaticLogGenerator::START_CHAT_DATE, $endDate);
