5e42da747a04e2d44ef1713f6674a54de6d6933b
borislav <b.manolov@gmail.com>
2023-04-06 12:24:36 +0200

    Use markdown syntax for chat postings
    It seems that the syntax <link|text> does not work for some reason.
    
    Use the markdown alternative.

--- posting.new_post_to_chat.mod.php
+++ posting.new_post_to_chat.mod.php
@@ -7,13 +7,14 @@ $rocketchatClient = new \Chitanka\RocketChatClient($rcCfg['url'], $rcCfg['userna
 $true_title = $post_data['forum_name'] .': ' . $post_data['post_subject'];
 // Remove all the http formatting from the topic_title
 $str_from = array('&lt;', '&gt;', '&#91;', '&#93;', '&#46;', '&#58;', '&#058;','&amp;', '&quot;');
-$str_to = array('[', ']', '[', ']', '.', ':', ':', '&', '"');
+$str_to = array('<', '>', '<', '>', '.', ':', ':', '&', '"');
 $clean_title = str_replace($str_from, $str_to, $true_title);
 
 $usernameClean = $user->data['user_id'] == ANONYMOUS
 	? (empty($post_data['username']) ? 'гост' : $post_data['username'])
 	: $user->data['username'];
+$postUrl = generate_board_url()."/post{$data['post_id']}.html#p{$data['post_id']}";
 $postForRocketChat = ($mode == 'edit' ? 'Променено мнение' : 'Ново мнение') . ': '
-	. "<".generate_board_url()."/post{$data['post_id']}.html#p{$data['post_id']} | {$clean_title}>"
+	. "[$clean_title]($postUrl)"
 	. ' от **'. $usernameClean .'**';
 $rocketchatClient->postMessage($postForRocketChat);
